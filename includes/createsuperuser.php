9383013c38e750abdb876a0f7b23ade70ac4bcff
Johan Andersson <900304@gmail.com>
2015-03-20 18:22:45 +0100

    Updated creation of superuser.

--- includes/createsuperuser.php
+++ includes/createsuperuser.php
@@ -3,11 +3,6 @@
 	$username = "root";
 	$password = "Schecter2011";
 	$dbname = "krustykookies";
-	
-	$servername = "localhost";
-	$userName = "root";
-	$password = "root";
-	$dbname = "krustykookies";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
@@ -17,7 +12,7 @@
 	} 
 	$password = password_hash("password", PASSWORD_DEFAULT);
 
-	$sql = "INSERT INTO users (userName, passWord) VALUES ('superuser', '".$password."')";
+	$sql = "INSERT INTO users (userName, passWord, isSuperUser) VALUES ('superuser', '".$password."', 1)";
 
 	if ($conn->query($sql) === TRUE) {
 		echo "New superuser created successfully";
